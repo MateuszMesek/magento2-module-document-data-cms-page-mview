b932fed0409af0989eb096f904ba4b7b394f27a7
Mateusz Mesek <mateusz@mesek.pl>
2022-05-10 22:47:34 +0200

    wip

--- src/SubscriptionProvider/Generator.php
+++ src/SubscriptionProvider/Generator.php
@@ -6,26 +6,34 @@ use InvalidArgumentException;
 use Magento\Cms\Api\Data\PageInterface;
 use Magento\Framework\DB\Ddl\Trigger;
 use Magento\Framework\EntityManager\MetadataPool;
+use Magento\Store\Model\ResourceModel\Store as StoreResource;
+use Magento\Store\Model\StoreDimensionProvider;
 use MateuszMesek\DocumentDataIndexMview\Data\SubscriptionFactory;
 use Traversable;
 
 class Generator
 {
     private MetadataPool $metadataPool;
+    private StoreResource $storeResource;
     private SubscriptionFactory $subscriptionFactory;
 
     public function __construct(
-        MetadataPool $metadataPool,
+        MetadataPool        $metadataPool,
+        StoreResource       $storeResource,
         SubscriptionFactory $subscriptionFactory
     )
     {
         $this->metadataPool = $metadataPool;
+        $this->storeResource = $storeResource;
         $this->subscriptionFactory = $subscriptionFactory;
     }
 
     public function generate(): Traversable
     {
         $metadata = $this->metadataPool->getMetadata(PageInterface::class);
+
+        $storeTable = $this->storeResource->getMainTable();
+        $storeDimensionName = StoreDimensionProvider::DIMENSION_NAME;
 
         foreach (Trigger::getListOfEvents() as $event) {
             switch ($event) {
@@ -45,15 +53,27 @@ class Generator
             yield $this->subscriptionFactory->create([
                 'tableName' => $metadata->getEntityTable(),
                 'triggerEvent' => $event,
-                'documentId' => "$prefix.{$metadata->getIdentifierField()}",
-                'dimensions' => "JSON_SET('{}', '$.scope', 0)",
+                'rows' => <<<SQL
+                    SELECT $prefix.{$metadata->getIdentifierField()} AS document_id,
+                           NULL AS node_path,
+                           JSON_SET('{}', '$.$storeDimensionName', store.store_id) AS dimensions
+                    FROM $storeTable AS store
+                    WHERE store.store_id != 0
+                SQL
             ]);
 
             yield $this->subscriptionFactory->create([
                 'tableName' => "{$metadata->getEntityTable()}_store",
                 'triggerEvent' => $event,
-                'documentId' => "(SELECT {$metadata->getIdentifierField()} FROM {$metadata->getEntityTable()} WHERE {$metadata->getLinkField()} = $prefix.{$metadata->getLinkField()})",
-                'dimensions' => "JSON_SET('{}', '$.scope', $prefix.store_id)",
+                'rows' => <<<SQL
+                    SELECT {$metadata->getIdentifierField()}  AS document_id,
+                           NULL AS node_path,
+                           JSON_SET('{}', '$.$storeDimensionName', store.store_id) AS dimensions
+                    FROM {$metadata->getEntityTable()}
+                    CROSS JOIN $storeTable AS store
+                        ON store.store_id != 0
+                    WHERE {$metadata->getLinkField()} = $prefix.{$metadata->getLinkField()}
+                SQL
             ]);
         }
     }
